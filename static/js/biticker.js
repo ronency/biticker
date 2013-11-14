a9d4b569d783ad3bc9dc1a1d5f88cae352570ebd
ronency <ronencypis@gmail.com>
2013-11-14 16:10:15 +0200

    added themes to the ticker

--- static/js/biticker.js
+++ static/js/biticker.js
@@ -1,62 +1,78 @@
 
 
-$(function(){
-  function trigger(event, data) {
-    $(document).trigger(event, data);
-  }
-  function bind(event, handler) {
-    $(document).bind(event, handler);
-  }
-
-  var symbolsRepo = {SGD: '$', ZAR: 'R',
-    USD: '$', AUD: '$', BRL: 'R$', CAD: '$', CNY: '¥',
-    CZK: 'Kč', EUR: '€', GBP: '£', ILS: '₪', JPY: '¥',
-    NOK: 'kr', NZD: '$', PLN: 'zł', RUB: 'RUB', SEK: 'kr'
+(function(){
+  var script = document.createElement("SCRIPT");
+  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js';
+  script.type = 'text/javascript';
+  document.getElementsByTagName("head")[0].appendChild(script);
+  var checkReady = function(callback) {
+    if (window.jQuery) {
+      callback(jQuery);
+    }
+    else {
+      window.setTimeout(function() { checkReady(callback); }, 20);
+    }
   };
+  checkReady(function($) {
+    function trigger(event, data) {
+      $(document).trigger(event, data);
+    }
+    function bind(event, handler) {
+      $(document).bind(event, handler);
+    }
 
-  var singleHtml =  '<span class="biticker-value">= #value#</span><span class="biticker-bids">Ask: #ask# &nbsp; Bid: #bid#</span>';
+    var symbolsRepo = {SGD: '$', ZAR: 'R',
+      USD: '$', AUD: '$', BRL: 'R$', CAD: '$', CNY: '¥',
+      CZK: 'Kč', EUR: '€', GBP: '£', ILS: '₪', JPY: '¥',
+      NOK: 'kr', NZD: '$', PLN: 'zł', RUB: 'RUB', SEK: 'kr'
+    };
 
-  var fetchDataForCurrency = function(currency){
-    $.ajax({
-      url: 'https://api.bitcoinaverage.com/ticker/'+currency+'?v=' + Math.random(),
-      type: 'GET',
-      dataType: 'json',
-      success: function(json){
-        updateTicker(currency, json);
-      }
+    var singleHtml =  '<div class="content"><span class="biticker-value">= #value#</span><span class="biticker-bids">Ask: #ask# &nbsp; Bid: #bid#</span></div>';
+
+    var fetchDataForCurrency = function(currency){
+      $.ajax({
+        url: 'https://api.bitcoinaverage.com/ticker/'+currency+'?v=' + Math.random(),
+        type: 'GET',
+        dataType: 'json',
+        success: function(json){
+          updateTicker(currency, json);
+        }
+      });
+    };
+
+    var createTicker = function(ticker){
+      var currency = ticker.data('currency');
+      fetchDataForCurrency(currency);
+      setInterval(function(){fetchDataForCurrency(currency);}, 10000);
+    };
+
+    var bindTicker = function(e, ticker){
+      ticker = $(ticker);
+      if(ticker.data('isBound'))
+        return;
+      createTicker(ticker);
+      ticker.data('isBound', true)
+    };
+
+    $('.biticker-container').each(function(){
+      var ticker = $(this);
+      if(ticker.data('isBound'))
+        return;
+      bindTicker(null, ticker);
     });
-  };
 
-  var createTicker = function(ticker){
-    var currency = ticker.data('currency');
-    fetchDataForCurrency(currency);
-    setInterval(function(){fetchDataForCurrency(currency);}, 10000);
-  };
+    bind('biticker:new-ticker-created', bindTicker);
 
-  var bindTicker = function(e, ticker){
-    ticker = $(ticker);
-    if(ticker.data('isBound'))
-      return;
-    createTicker(ticker);
-    ticker.data('isBound', true)
-  };
-
-  $('.biticker-container').each(function(){
-    var ticker = $(this);
-    if(ticker.data('isBound'))
-      return;
-    bindTicker(null, ticker);
+    var updateTicker = function(currency, data){
+      var ticker = $('[data-currency='+currency+']');
+      var symbol = symbolsRepo[currency];
+      var html =  singleHtml.replace('#value#', symbol + data.last)
+                            .replace('#ask#', symbol + data.ask)
+                            .replace('#bid#', symbol + data.bid);
+      ticker.addClass('biticker-loading')
+            .html(html)
+            .removeClass('biticker-loading');
+    };
   });
-
-  bind('biticker:new-ticker-created', bindTicker);
-
-  var updateTicker = function(currency, data){
-    var ticker = $('[data-currency='+currency+']');
-    var symbol = symbolsRepo[currency];
-    var html =  singleHtml.replace('#value#', symbol + data.last)
-                          .replace('#ask#', symbol + data.ask)
-                          .replace('#bid#', symbol + data.bid);
-    ticker.html(html);
-  };
-});
+})();
 
