ab355ad8632f4672d34c0f0ed7c214a0e0e7afba
ronency <ronencypis@gmail.com>
2013-11-16 00:45:25 +0200

    changed URL for the biticker.css file to served from

--- static/js/biticker.js
+++ static/js/biticker.js
@@ -14,6 +14,7 @@
     }
   };
   checkReady(function($) {
+    $('<link />', {rel:'stylesheet', type:'text/css', href: 'http://ronency.github.io/biticker/stylesheets/biticker.css'}).appendTo('head');
     function trigger(event, data) {
       $(document).trigger(event, data);
     }
