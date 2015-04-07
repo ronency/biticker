b87fa770d8c7416b0bfc437d3dd235c2e4a70765
ronency <ronencypis@gmail.com>
2015-04-08 02:22:53 +0300

    changed the interval for 90 secs

--- javascripts/biticker.js
+++ javascripts/biticker.js
@@ -44,7 +44,7 @@
     var createTicker = function(ticker){
       var currency = ticker.data('currency');
       fetchDataForCurrency(currency);
-      setInterval(function(){fetchDataForCurrency(currency);}, 30000);
+      setInterval(function(){fetchDataForCurrency(currency);}, 90000);
     };
 
     var bindTicker = function(e, ticker){
